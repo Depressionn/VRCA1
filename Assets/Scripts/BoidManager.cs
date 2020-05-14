3a97ccebd0a61f3f8acab8b3ec6b7062f67bdcb7
Depressionn <tantiongguan123@gmail.com>
2020-05-14 14:44:11 +0800

    Boids can spawn now

--- Assets/Scripts/BoidManager.cs
+++ Assets/Scripts/BoidManager.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BoidManager : Singleton<BoidManager> {
     public GameObject boidPfb;
     public int spawnCount = 50;
-    public float spawnRadius = 20;
 
     // Start is called before the first frame update
     void Start() {
         for(int count = 0; count < spawnCount; count++) {
-            Spawn(transform.position + Random.insideUnitSphere * spawnRadius);
+            NavMeshHit hit;
+            if(NavMesh.SamplePosition(Random.insideUnitSphere * 100 + transform.position, out hit, 100, 1))
+                Spawn(hit.position);
         }
     }
 
@@ -22,10 +24,5 @@ public class BoidManager : Singleton<BoidManager> {
     private GameObject Spawn(Vector3 _position) {
         _position.y = 0;
         return Instantiate(boidPfb, _position, Quaternion.Euler(0, Random.Range(0, 360), 0));
-    }
-
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 }
